a98dd37b768af9e98ec27408ad280856502d4c14
Anders Wind Steffensen <awia00@gmail.com>
2014-02-04 22:36:04 +0100

    Added custom spheres to the scene in the ray tracer main class

--- RayTracer/src/raytracer/RayTracer.java
+++ RayTracer/src/raytracer/RayTracer.java
@@ -20,7 +20,10 @@ public class RayTracer {
     public static void main(String[] args) {
         Scene scene = new Scene(new SimpleOptCamera(20, 20, 20, 800, 600));
         scene.setBackgroundColor(Color.darkGray);
-        scene.addVirtualObject(new VirtualObjectPlane(new Position3d(5,5,5), new Vector3d(2,2,2), new ShaderDiffuse(Color.orange)));
+        //scene.addVirtualObject(new VirtualObjectPlane(new Position3d(5,5,5), new Vector3d(2,2,2), new ShaderDiffuse(Color.orange)));
+        scene.addVirtualObject(new VirtualObjectSphere(new Position3d(50,200,50),20,new ShaderDiffuse(Color.BLUE)));
+        scene.addVirtualObject(new VirtualObjectSphere(new Position3d(20,150,-20),25,new ShaderDiffuse(Color.red)));
+        scene.addVirtualObject(new VirtualObjectSphere(new Position3d(0,200,-50),30,new ShaderDiffuse(Color.orange)));
         scene.createRays();
     }
     
