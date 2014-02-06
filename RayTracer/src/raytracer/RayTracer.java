f551c1424d9600122c70a508909239f27accf4c9
Anders Wind <awia00@gmail.com>
2014-02-06 17:35:05 +0100

    The main class now takes reflectivity into account

--- RayTracer/src/raytracer/RayTracer.java
+++ RayTracer/src/raytracer/RayTracer.java
@@ -19,16 +19,22 @@ public class RayTracer {
      */
     public static void main(String[] args) {
         Scene scene = new Scene(new Camera_SimpleOpt(25, 50, 40, 800, 600));
-        scene.setBackgroundColor(new Color(10,10,10));
-        scene.setAmbientLight(new Color(13,13,13));
+        scene.setBackgroundColor(new Color(10,10,25));
+        scene.setAmbientLight(new Color(13,13,30));
+        scene.setLevelOfRays(3);
         //scene.addVirtualObject(new VirtualObject_Plane(new Position3d(5,500,5), new Vector3d(2,2,2), new ShaderDiffuse(Color.GREEN)));
-        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(75,200,50),40,new ShaderDiffuse(Color.BLUE)));
-        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(20,150,-40),35,new ShaderSpecular(Color.red, Color.white, 100)));
-        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(0,400,-50),50,new ShaderDiffuse(Color.orange)));
-        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(-25,250,10),10,new ShaderSpecular(Color.CYAN, Color.white, 100)));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(75,200,50),40,new ShaderDiffuse(Color.BLUE),true,0.9));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(20,150,-40),35,new ShaderSpecular(Color.red, Color.white, 100),true,0.5));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(0,400,-50),50,new ShaderDiffuse(Color.orange),true,0.5));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(-25,250,10),10,new ShaderSpecular(Color.CYAN, Color.white, 100),true,0.5));
+        //
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(-35,350,10),30,new ShaderSpecular(Color.CYAN, Color.white, 100),true,0.5));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(2,250,50),10,new ShaderSpecular(Color.CYAN, Color.white, 100),true,0.5));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(-12,250,33),10,new ShaderSpecular(Color.CYAN, Color.white, 100),true,0.5));
+        scene.addVirtualObject(new VirtualObject_Sphere(new Position3d(-43,250,98),10,new ShaderSpecular(Color.CYAN, Color.white, 100),true,0.5));
         
         scene.addLightObject(new LightObject_Directional(new Vector3d(-0.7,-0.7,0).normalize(),1));
-        //scene.addLightObject(new LightObject_Directional(new Vector3d(-1,1,0),0.9));
+        scene.addLightObject(new LightObject_Directional(new Vector3d(-1,1,0),0.9));
         //scene.addLightObject(new LightObject_Directional(new Vector3d(5,-2,-2),0.2));
         scene.createRays();
     }
