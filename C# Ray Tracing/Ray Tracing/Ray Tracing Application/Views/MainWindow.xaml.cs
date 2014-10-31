079af578a7ce22b9f2c995ead3e31ba69504e686
Anders Wind Steffensen <awis@itu.dk>
2014-10-31 12:19:49 +0100

    Updated the canvas and found a method for creating a bitmap from pixel array

--- C# Ray Tracing/Ray Tracing/Ray Tracing Application/Views/MainWindow.xaml.cs
+++ C# Ray Tracing/Ray Tracing/Ray Tracing Application/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Ray_Tracing_Application.Views
 {
@@ -10,6 +13,32 @@ namespace Ray_Tracing_Application.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void drawCanvas()
+        {
+            //int width = 200;
+            //int height = 200;
+
+            ////
+            //// Here is the pixel format of your data, set it to the proper value for your data
+            ////
+            //PixelFormat pf = PixelFormats.Bgr32;
+            //int rawStride = (width * pf.BitsPerPixel + 7) / 8;
+
+            ////
+            //// Here is your raw data
+            ////
+            //int[] rawImage = new int[rawStride * height / 4];
+
+
+            ////
+            //// Create the BitmapSource
+            ////
+            //BitmapSource bitmap = BitmapSource.Create(
+            //    width, height,
+            //    96, 96, pf, null,
+            //    rawImage, rawStride);
         }
     }
 }
