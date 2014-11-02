f2d411104d30a3406920cde1236deca8974c0647
Anders Wind Steffensen <awis@itu.dk>
2014-11-02 13:03:04 +0100

    Created an ambient light and added intensity and color to the light interface

---
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Lights/AmbientLight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingModel.Model.Lights
+{
+    public class AmbientLight : ILight
+    {
+        public double Intensity { get; set; }
+        public Color Color { get; set; }
+        public Vector3D CalculateLightDirectionOnPosition(Vector3D positionVector3D)
+        {
+            // find out what to do here since abient light does not have vectors.
+            return new Vector3D();
+        }
+
+        public AmbientLight(double intensity, Color color)
+        {
+            Intensity = intensity;
+            Color = color;
+        }
+    }
+}
--- C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Lights/ILight.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Lights/ILight.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RayTracingModel.Model.Lights
 {
-    interface ILight
+    public interface ILight
     {
+        double Intensity { get; set; }
+        Color Color { get; set; }
+        Vector3D CalculateLightDirectionOnPosition(Vector3D positionVector3D);
     }
 }
