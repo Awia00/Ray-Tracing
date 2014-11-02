c2d14f2230f63ec763e78b1a716a6029867b2425
Anders Wind Steffensen <awia00@gmail.com>
2014-11-02 22:15:31 +0100

    Added a directional light

---
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Lights/DirectionalLight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingModel.Model.Lights
+{
+    class DirectionalLight : ILight
+    {
+        public double Intensity { get; set; }
+        public Color Color { get; set; }
+        private Vector3D _direction;
+        public Vector3D CalculateLightDirectionOnPosition(Vector3D positionVector3D)
+        {
+            return _direction;
+        }
+        public DirectionalLight(Vector3D direction, double intensity, Color color)
+        {
+            _direction = direction;
+            Intensity = intensity;
+            Color = color;
+        }
+    }
+}
