47e3889223796e79785d4b1ece231c462222dd25
Anders Wind Steffensen <awis@itu.dk>
2014-11-02 14:37:29 +0100

    Used the subtraction method from Vector3D in camera to improve readability

--- C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Cameras/SimpleCamera.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Cameras/SimpleCamera.cs
@@ -23,9 +23,9 @@ namespace RayTracingModel.Model.Cameras
             {
                 for (int j = 0; j < AmtOfWidthPixels; j++)
                 {
-                    var tempDirectionVector = new Vector3D(-Width/2 + i*(Width/(double)AmtOfWidthPixels) - Eye.X, 
-                        Direction.Length + Eye.Y,
-                        -Height/2 + j*(Height/(double)AmtOfHeightPixels)-Eye.Z);
+                    var tempDirectionVector = Vector3D.Subtraction(new Vector3D(-Width/2 + i*(Width/(double)AmtOfWidthPixels), 
+                        Direction.Length,
+                        -Height/2 + j*(Height/(double)AmtOfHeightPixels)) , Eye);
                     tempCameraVectors[i,j] = new Line3D(Eye, tempDirectionVector);
                 }
             }
