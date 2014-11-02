38611d6260dff5d1647ebd3b6fea39ec634b02cc
Anders Wind Steffensen <awis@itu.dk>
2014-11-02 14:37:55 +0100

    Improved IObject3D

--- C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Objects3D/IObject3D.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Objects3D/IObject3D.cs
@@ -1,6 +1,15 @@
-﻿namespace RayTracingModel.Model.Objects3D
+﻿using System.Collections.Generic;
+using System.Drawing;
+using RayTracingModel.Model.Lights;
+using RayTracingModel.Model.Shaders;
+
+namespace RayTracingModel.Model.Objects3D
 {
-    interface IObject3D
+    public interface IObject3D
     {
+        IShader Shader { get; set; }
+        Vector3D CenterPositionVector3D { get; set; }
+        Color CalculateColor(IList<ILight> lightsThatHitsSurface, Vector3D positionVector3D);
+        double CalculateCollisionPosition(Line3D line3D);
     }
 }
