91461ec6087ddac157a1195ab18d76b252f415f8
Anders Wind Steffensen <awis@itu.dk>
2014-11-03 17:09:37 +0100

    Started creating the specular component.

--- C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
@@ -30,7 +30,7 @@ namespace RayTracingModel
             _scene.AmtOfRecoursions = 3;
 
             // Camera
-            _scene.Camera = new SimpleCamera(new Vector3D(2, 2, 2), new Vector3D(0, 0, 0), 16, 10, 1280, 800);
+            _scene.Camera = new SimpleCamera(new Vector3D(2, 2, 2), new Vector3D(0, 0, 0), 16, 10, 1920, 1080);
 
             // Lights
             _scene.SceneLights.Add(new AmbientLight(0.1,Color.FromArgb(200,200,250)));
@@ -38,9 +38,9 @@ namespace RayTracingModel
             _scene.SceneLights.Add(new DirectionalLight(new Vector3D(-1, 2, -1), 0.5, Color.FromArgb(200, 200, 250)));
 
             //Objects
-            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.White,0.5,0), new Vector3D(-5,15,0),5));
-            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.ForestGreen, 0.1, 0), new Vector3D(0, 25, 5), 10));
-            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.MediumBlue, 0.2, 0), new Vector3D(5, 12, -8), 5));
+            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(0.7,0, Color.Firebrick,Color.White), new Vector3D(-5,15,0),5));
+            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.ForestGreen, 0, 0), new Vector3D(0, 25, 5), 10));
+            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.MediumBlue, 0, 0), new Vector3D(5, 12, -8), 5));
         }
 
         public static Controller GetInstance()
---
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Model/Shaders/SpecularShader.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color_Toolbox;
+using RayTracingModel.Model.Lights;
+
+namespace RayTracingModel.Model.Shaders
+{
+    public class SpecularShader : IShader
+    {
+        public double Reflectivity { get; set; }
+        public double Refractivity { get; set; }
+        public bool IsReflective()
+        {
+            if (Reflectivity > 0) return true;
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsRefractive()
+        {
+            if (Reflectivity > 0) return true;
+            else
+            {
+                return false;
+            }
+        }
+        private void CheckInvariants()
+        {
+            if (Reflectivity < 0 || Reflectivity < 0) throw new Exception("Neither reflectivity or refractivity must be beneath 0");
+            if (Reflectivity > 1 || Reflectivity > 1) throw new Exception("Neither reflectivity or refractivity must be over 1");
+        }
+
+        public SpecularShader(double reflectivity, double refractivity, Color diffuseColor, Color specularColor)
+        {
+            Reflectivity = reflectivity;
+            Refractivity = refractivity;
+            DiffuseColor = diffuseColor;
+            SpecularColor = specularColor;
+        }
+
+        public Color DiffuseColor { get; set; }
+        public Color SpecularColor { get; set; }
+        public double SpecularComponent { get; set; }
+
+        public Color CalculateColor(IList<ILight> lightsThatHitsSurface, Vector3D normalVector3D, Vector3D rayVector3D,
+            Vector3D collisionPositionVector3D)
+        {
+            if (lightsThatHitsSurface.Count == 0) throw new Exception();
+            // todo refacter functionality into methods
+            Color baseColor = new Color();
+            Color ambientColor = GenerateAmbientColor(lightsThatHitsSurface);
+            Color diffuseColor = GenerateDiffuseColor(lightsThatHitsSurface, normalVector3D, collisionPositionVector3D);
+            Color specularColor = GenerateSpecularColor(lightsThatHitsSurface, normalVector3D, rayVector3D,
+                collisionPositionVector3D);
+
+            baseColor = ColorToolbox.BlendAddition(baseColor, ambientColor);
+            baseColor = ColorToolbox.BlendAddition(baseColor, diffuseColor);
+            baseColor = ColorToolbox.BlendAddition(baseColor, specularColor);
+
+            return baseColor;
+        }
+
+        private Color GenerateAmbientColor(IList<ILight> lightsThatHitsSurface)
+        {
+            foreach (var light in lightsThatHitsSurface)
+            {
+                if (light is AmbientLight)
+                {
+                    return ColorToolbox.ColorIntensify(light.Color, light.Intensity);
+                }
+            }
+            return new Color();
+        }
+
+        private Color GenerateDiffuseColor(IList<ILight> lightsThatHitsSurface, Vector3D normalVector3D, Vector3D collisionPositionVector3D)
+        {
+            double intensity = 0;
+            foreach (var light in lightsThatHitsSurface)
+            {
+                intensity += light.Intensity * (Math.Max(0, Vector3D.DotProdukt(normalVector3D.VectorNegation(), light.CalculateLightDirectionOnPosition(collisionPositionVector3D))));
+            }
+            return ColorToolbox.ColorIntensify(DiffuseColor, intensity);
+        }
+
+        private Color GenerateSpecularColor(IList<ILight> lightsThatHitsSurface, Vector3D normalVector3D, Vector3D rayVector3D,
+            Vector3D collisionPositionVector3D)
+        {
+            double intensity = 0;
+            Vector3D negDirectionVector = rayVector3D.VectorNegation();
+            foreach (var light in lightsThatHitsSurface)
+            {
+                Vector3D hVector = Vector3D.Addition(negDirectionVector.Normalize(), light.CalculateLightDirectionOnPosition(collisionPositionVector3D).Normalize());
+                intensity += light.Intensity * Math.Pow(Math.Max(0, Vector3D.DotProdukt(hVector.Normalize(), normalVector3D.VectorNegation().Normalize())), SpecularComponent);
+            }
+            return ColorToolbox.ColorIntensify(SpecularColor, intensity);
+        }
+
+
+    }
+}
