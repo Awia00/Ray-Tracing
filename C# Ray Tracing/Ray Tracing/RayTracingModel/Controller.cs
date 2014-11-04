827de2d7a11d1c63bf1e572ae1565afa1c7271fe
Anders Wind Steffensen <awia00@gmail.com>
2014-11-04 23:10:58 +0100

    Fixed a little view bug

--- C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
@@ -33,7 +33,7 @@ namespace RayTracingModel
             // Settings
             _scene.Settings.BackgroundColor = Color.FromArgb(100, 130, 155);
             _scene.Settings.AmtOfRecoursions = 3;
-            _scene.Settings.ShadowRays = 128;
+            _scene.Settings.ShadowRays = 32;
 
             // Camera
             //_scene.Camera = new SimpleCamera(new Vector3D(3, 3, 3), new Vector3D(0, 0, 2), 16, 10, 640, 400); // low res 16 10 ratio
