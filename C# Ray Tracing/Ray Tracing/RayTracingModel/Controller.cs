7244e17ab604eb704a88ce8f72691c0ef26a6a4c
Anders Wind Steffensen <awia00@gmail.com>
2014-11-13 23:53:06 +0100

    Improved the scenes by including walls and changing camera position

--- C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
+++ C# Ray Tracing/Ray Tracing/RayTracingModel/Controller.cs
@@ -29,31 +29,56 @@ namespace RayTracingModel
         {
             // Settings
             _scene.Settings.BackgroundColor = Color.FromArgb(100, 130, 155);
-            _scene.Settings.AmtOfRecoursions = 2;
-            _scene.Settings.ShadowRays = 150;
+            _scene.Settings.AmtOfRecoursions = 3;
+            _scene.Settings.ShadowRays = 0;
 
             // Camera
-            //_scene.Camera = new SimpleCamera(new Vector3D(3, 3, 3), new Vector3D(0, 0, 2), 16, 10, 640, 400); // low res 16 10 ratio
-            _scene.Camera = new SimpleCamera(new Vector3D(3, 3, 3), new Vector3D(0, 0, 2), 16, 10, 960, 600); // medium res 16 10 ratio
-            //_scene.Camera = new SimpleCamera(new Vector3D(3, 3, 3), new Vector3D(0, 0, 2), 16, 10, 1280, 800); // HD 16 10 ratio
-            //_scene.Camera = new SimpleCamera(new Vector3D(3, 3, 3), new Vector3D(0, 0, 2), 16, 10, 1920, 1200); // Full HD 16 10 ratio
+            //_scene.Camera = new SimpleCamera(new Vector3D(4, 4, 4), new Vector3D(0, -5, 7), 16, 10, 640, 400); // low res 16 10 ratio
+            _scene.Camera = new SimpleCamera(new Vector3D(4, 4, 4), new Vector3D(0, -5, 7), 16, 10, 960, 600); // medium res 16 10 ratio
+            //_scene.Camera = new SimpleCamera(new Vector3D(4, 4, 4), new Vector3D(0, -5, 7), 16, 10, 1280, 800); // HD 16 10 ratio
+            //_scene.Camera = new SimpleCamera(new Vector3D(4, 4, 4), new Vector3D(0, -5, 7), 16, 10, 1920, 1200); // Full HD 16 10 ratio
 
             // Lights
             _scene.SceneLights.Add(new AmbientLight(0.1,Color.Wheat));
            // _scene.SceneLights.Add(new DirectionalLight(new Vector3D(0.5,-0.1,-0.5), 0.8, Color.Wheat));
-            //_scene.SceneLights.Add(new DirectionalLight(new Vector3D(-0.1, 1, 0.1), 0.5, Color.Wheat));
+            _scene.SceneLights.Add(new DirectionalLight(new Vector3D(-0.1, 1, 0.1), 0.5, Color.Wheat));
             _scene.SceneLights.Add(new LocalLight(0.8, Color.Wheat, new Vector3D(0,8,-2)));
 
             //Objects
-            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.Silver, Color.White, 10, 0.3, 0, 1), new Vector3D(-10,15,0),5));
-            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.ForestGreen, 0, 0.7, 1.33), new Vector3D(0, 25, 5), 10));
-            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.ForestGreen, Color.White, 10, 0, 0, 1), new Vector3D(75, 100, 5), 10));
-            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.MediumBlue, Color.White, 5, 0, 0.5, 1.2), new Vector3D(10, 20, -2), 3));
+            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.Silver, Color.White, 10,        0.3, 0, 1),    new Vector3D(-10,15,0),5));
+
+            _scene.SceneObjects.Add(new SphereObject3D(new DiffuseShader(Color.ForestGreen,                     0, 0, 1.2),   new Vector3D(0, 25, 5), 10));
+
+            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.MediumBlue, Color.White, 5,     0, 0, 1.2),    new Vector3D(10, 20, -2), 3));
+
+            _scene.SceneObjects.Add(new SphereObject3D(new SpecularShader(Color.Red, Color.White, 10,           0, 0, 1),      new Vector3D(0, 40, 5), 8));
+            
             _scene.SceneObjects.Add(new PlaneObject3D()
             {
-                Shader = new DiffuseShader(Color.Goldenrod, 0.2, 0, 1),
+                Shader = new DiffuseShader(Color.Goldenrod, 0, 0, 1),
                 CenterPositionVector3D = new Vector3D(0, 5, -5),
-                NormalVector3D = new Vector3D(0, 0, -1).VectorNegation()
+                NormalVector3D = new Vector3D(0, 0, 1)
+            });
+
+            _scene.SceneObjects.Add(new PlaneObject3D()
+            {
+                Shader = new DiffuseShader(Color.Wheat, 0, 0, 1),
+                CenterPositionVector3D = new Vector3D(0, 50, 0),
+                NormalVector3D = new Vector3D(0, -1, 0)
+            });
+
+            _scene.SceneObjects.Add(new PlaneObject3D()
+            {
+                Shader = new DiffuseShader(Color.CornflowerBlue, 0, 0, 1),
+                CenterPositionVector3D = new Vector3D(-25, 0, -0),
+                NormalVector3D = new Vector3D(1, 0, 0)
+            });
+
+            _scene.SceneObjects.Add(new PlaneObject3D()
+            {
+                Shader = new DiffuseShader(Color.IndianRed, 0, 0, 1),
+                CenterPositionVector3D = new Vector3D(25, 0, 0),
+                NormalVector3D = new Vector3D(-1, 0, 0)
             });
         }
 
